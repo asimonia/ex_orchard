ab01bd489766e2fe6897e4e5831764dee1d0b756
asimonia <alex.simonian@gmail.com>
2016-11-14 19:58:07 -0600

    Complete

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let orchard = document.getElementById("orchard");
 let Plant = function() {
 	this.height = null;
 	this.branches = null;
+	this.counter = null;
 };
 
 let Tree = function() {
@@ -21,12 +22,18 @@ Plant.prototype.decreaseHeight = function (growth) {
 };
 
 Tree.prototype.grow = function(growth) {
-	this.height += growth;
+	this.increaseHeight(growth);
+	this.counter += growth;
+
+	while(this.counter >= 10) {
+		this.counter -= 10;
+		this.branches++;
+	}
 };
 
 Tree.prototype.trim = function(growth) {
-	this.height -= growth;
-	this.branches -= 1;
+	this.decreaseHeight(growth);
+	this.branches--;
 };
 
 
@@ -38,18 +45,22 @@ var PearTree = new Tree();
 var OakTree = new Tree();
 
 
-window.setInterval(function() {
-	if (counter % 10 === 0 && counter !== 0) {
-		OakTree.trim(4);
-		PearTree.trim(2);
+let orchardTimer = setInterval(function() {
+
+	if (counter % 10 === 0 && counter != 0) {
+		OakTree.trim(3);
+		PearTree.trim(1);
+	} else {
+		OakTree.grow(4);
+		PearTree.grow(2);
 	}
 
+	counter ++;
+
 	if (counter === 30) {
-		clearInterval();
+		clearInterval(orchardTimer);
 	}
-	counter += 1;
-	OakTree.grow(4);
-	PearTree.grow(2);
-	orchard.innerHTML += `<p>Pear tree is now ${PearTree.height}cm tall and has ${PearTree.branches} branches.`;
+	
+	orchard.innerHTML += `<p>${counter}: Pear tree is now ${PearTree.height}cm tall and has ${PearTree.branches} branches.`;
 	orchard.innerHTML += `Oak tree is now ${OakTree.height}cm tall and has ${OakTree.branches} branches.</p>`;
-}, 1000);
+}, 100);
